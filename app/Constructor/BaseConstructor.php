a7a9d09c3f1cecfcb92342949475166cce1a8b19
John <john@custom-golf-bags.com>
2019-08-17 17:24:20 +0700

    Modified

--- app/Constructor/BaseConstructor.php
+++ app/Constructor/BaseConstructor.php
@@ -18,17 +18,17 @@ use Interop\Container\ContainerInterface;
 
 abstract class BaseConstructor {
 	
-	protected $container;
-	
-	public function __construct(ContainerInterface $container) {
-		$this->container = $container;
-	}
-	
-	public function __get($property) {
-		if($this->container->{$property}) {
-			return $this->container->{$property};
-		}
-	}
+    protected $container;
+
+    public function __construct(ContainerInterface $container) {
+        $this->container = $container;
+    }
+
+    public function __get($property) {
+        if($this->container->{$property}) {
+            return $this->container->{$property};
+        }
+    }
 	
 }
 
