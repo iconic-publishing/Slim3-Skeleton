9e5f3f5cfa8ae0787653739006878cadf19f6b76
John <john@custom-golf-bags.com>
2019-08-17 17:44:24 +0700

    Modified

--- app/Controllers/Auth/AuthLoginController.php
+++ app/Controllers/Auth/AuthLoginController.php
@@ -15,11 +15,11 @@ Change Request ID:
 namespace Base\Controllers\Auth;
 
 use Base\{
-	Constructor\BaseConstructor,
-	Validation\Forms\Auth\AuthForm,
-	Models\User\User,
-	Helpers\Session,
-	Helpers\Cookie
+    Constructor\BaseConstructor,
+    Validation\Forms\Auth\AuthForm,
+    Models\User\User,
+    Helpers\Session,
+    Helpers\Cookie
 };
 use Psr\Http\Message\{
     ServerRequestInterface as Request,
@@ -34,74 +34,66 @@ class AuthLoginController extends BaseConstructor {
     }
 
     public function postLogin(Request $request, Response $response) {
-		$validation = $this->validator->validate($request, AuthForm::loginRules());
+        $validation = $this->validator->validate($request, AuthForm::loginRules());
 
-		if($validation->fails()) {
-			$this->flash->addMessage('error', $this->config->get('messages.login.error'));
-			return $response->withRedirect($this->router->pathFor('getLogin'));
-		}
+        if($validation->fails()) {
+            $this->flash->addMessage('error', $this->config->get('messages.login.error'));
+            return $response->withRedirect($this->router->pathFor('getLogin'));
+        }
 
-		$identifier = $request->getParam('email_or_username');
-		$email_address = $request->getParam('email_address');
-		$password = $request->getParam('password');
-		$remember = $request->getParam('remember');
+        $identifier = $request->getParam('email_or_username');
+        $email_address = $request->getParam('email_address');
+        $password = $request->getParam('password');
 
-		$user = User::where(function($query) use ($identifier) {
-			return $query->where('email_address', $identifier)->orWhere('username', $identifier);
-		})->first();
-		
-		if(!$user->recover_hash == null) {
-			$this->flash->addMessage('warning', $this->config->get('messages.login.passwordReset'));
-			return $response->withRedirect($this->router->pathFor('getLogin'));
-		}
+        $user = User::where(function($query) use ($identifier) {
+            return $query->where('email_address', $identifier)->orWhere('username', $identifier);
+        })->first();
 
-		if(!$user || !$this->hash->passwordCheck($password, $user->password)) {
-			$this->flash->addMessage('error', $this->config->get('messages.login.notUser'));
-			return $response->withRedirect($this->router->pathFor('getLogin'));
-		}
+        if($user->recover_hash) {
+            $this->flash->addMessage('warning', $this->config->get('messages.login.passwordReset'));
+            return $response->withRedirect($this->router->pathFor('getLogin'));
+        }
 
-		if(!$user->active == true) {
-			$this->flash->addMessage('warning', $this->config->get('messages.login.notActive'));
-			return $response->withRedirect($this->router->pathFor('getLogin'));
-		}
+        if(!$user || !$this->hash->passwordCheck($password, $user->password)) {
+            $this->flash->addMessage('error', $this->config->get('messages.login.notUser'));
+            return $response->withRedirect($this->router->pathFor('getLogin'));
+        }
 
-		if(!$user->locked == false) {
-			$this->flash->addMessage('warning', $this->config->get('messages.login.locked'));
-			return $response->withRedirect($this->router->pathFor('getLogin'));
-		}
+        if(!$user->active) {
+            $this->flash->addMessage('warning', $this->config->get('messages.login.notActive'));
+            return $response->withRedirect($this->router->pathFor('getLogin'));
+        }
 
-		if($user && $this->hash->passwordCheck($password, $user->password)) {
-			Session::put('user', $user->id);
+        if($user->locked) {
+            $this->flash->addMessage('warning', $this->config->get('messages.login.locked'));
+            return $response->withRedirect($this->router->pathFor('getLogin'));
+        }
 
-			$size = $this->config->get('auth.token');
-			$token = $this->hash->hashed($size);
+        if($user && $this->hash->passwordCheck($password, $user->password)) {
+            Session::put('user', $user->id);
 
-			$user->createLoginToken($token);
+            $size = $this->config->get('auth.token');
+            $token = $this->hash->hashed($size);
+            $user->createLoginToken($token);
 
-			if($remember === 'on') {
-				$rememberIdentifier = $this->hash->hashed($size);
-				$rememberToken = $this->hash->hashed($size);
+            if($request->getParam('remember') === 'on') {
+                $rememberIdentifier = $this->hash->hashed($size);
+                $rememberToken = $this->hash->hashed($size);
 
-				$user->updateRememberCredentials(
-					$rememberIdentifier,
-					$rememberToken
-				);
+                $user->updateRememberCredentials($rememberIdentifier, $rememberToken);
 
-				Cookie::put($this->config->get('auth.remember'), 
-					$rememberIdentifier . '___' . $rememberToken, 
-					Carbon::parse('+1 month')->timestamp
-				);
-			}
+                Cookie::put($this->config->get('auth.remember'), $rememberIdentifier . '___' . $rememberToken, Carbon::parse('+1 month')->timestamp);
+            }
 
-			if($this->auth->user()->isGroup()) {
-				return $response->withRedirect($this->router->pathFor('admin', compact('token')));
-			} else {
-				return $response->withRedirect($this->router->pathFor('member', compact('token')));
-			}
-		} else {
-			$this->flash->addMessage('warning', $this->config->get('messages.login.notActive'));
-			return $response->withRedirect($this->router->pathFor('getLogin'));
-		}
+            if($this->auth->user()->isGroup()) {
+                return $response->withRedirect($this->router->pathFor('admin', compact('token')));
+            } else {
+                return $response->withRedirect($this->router->pathFor('member', compact('token')));
+            }
+        } else {
+            $this->flash->addMessage('warning', $this->config->get('messages.login.notActive'));
+            return $response->withRedirect($this->router->pathFor('getLogin'));
+        }
     }
 
 }
