2e936f65693cacbe9316bb1bc7919ff5d0bdd288
John <john@custom-golf-bags.com>
2019-08-17 19:10:32 +0700

    Modified

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -19,121 +19,121 @@ use Base\Models\User\UserPermission;
 
 class User extends Model {
 	
-	protected $table = 'users';
+    protected $table = 'users';
 
     protected $fillable = [
-		'username',
-		'email_address',
-		'first_name',
-		'last_name',
-		'mobile_number',
-		'password',
-		'token',
-		'active',
-		'locked',
-		'active_hash',
-		'recover_hash',
-		'remember_identifier',
-		'remember_token'
-	];
-	
-	public function permissions() {
-		return $this->hasOne(UserPermission::class, 'user_id');
-	}
-	
-	public function hasPermission($permission) {
-		return (bool) $this->permissions->{$permission};
-	}
-	
-	public function isAdministrator() {
-		return $this->hasPermission('is_administrator');
-	}
-	
-	public function isAdmin() {
-		return $this->hasPermission('is_admin');
-	}
-	
-	public function isStaff() {
-		return $this->hasPermission('is_staff');
-	}
-	
-	public function isUser() {
-		return $this->hasPermission('is_user');
-	}
-	
-	public function isGroup() {
-		if($this->isAdministrator() || $this->isAdmin() || $this->isStaff()) {
-			return $this;
-		}
-	}
-	
-	public function isUserType() {
-		if($this->isAdministrator()) {
-			return 'You are an Administrator';
-		} else if($this->isAdmin()) {
-			return 'You are Admin Staff';
-		} else if($this->isStaff()) {
-			return 'You are Staff';
-		} else {
-			return 'You are a Member';
-		}
-	}
-	
-	public function activateAccount() {
-		$this->update([
-			'active' => true,
-			'locked' => false,
-			'active_hash' => null
-		]);
-	}
+        'username',
+        'email_address',
+        'first_name',
+        'last_name',
+        'mobile_number',
+        'password',
+        'token',
+        'active',
+        'locked',
+        'active_hash',
+        'recover_hash',
+        'remember_identifier',
+        'remember_token'
+    ];
 
-	public function updateRememberCredentials($identifier, $token) {
-		$this->update([
-			'remember_identifier' => $identifier,
-			'remember_token' => $token
-		]);
-	}
+    public function permissions() {
+        return $this->hasOne(UserPermission::class, 'user_id');
+    }
 
-	public function removeRememberCredentials() {
-		$this->updateRememberCredentials(null, null);
-	}
-	
-	public function createLoginToken($token) {
-		$this->update([
-			'token' => $token
-		]);
-	}
-	
-	public function removeLoginToken() {
-		$this->createLoginToken(null);
-	}
+    public function hasPermission($permission) {
+        return (bool) $this->permissions->{$permission};
+    }
 
-	public function getFirstName() {
-		if($this->first_name) {
-			return $this->first_name;
-		}
-		
-		return null;
-	}
-	
-	public function getFullName() {
-		if($this->first_name && $this->last_name) {
-			return "{$this->first_name} {$this->last_name}";
-		}
-		
-		if($this->first_name) {
-			return $this->first_name;
-		}
-		
-		return null;
-	}
-	
-	public function getNameOrUsername() {
-		return $this->getFullName() ?: $this->username;
-	}
-	
-	public function getFirstNameOrUsername() {
-		return $this->first_name ?: $this->username;
-	}
+    public function isAdministrator() {
+        return $this->hasPermission('is_administrator');
+    }
+
+    public function isAdmin() {
+        return $this->hasPermission('is_admin');
+    }
+
+    public function isStaff() {
+        return $this->hasPermission('is_staff');
+    }
+
+    public function isUser() {
+        return $this->hasPermission('is_user');
+    }
+
+    public function isGroup() {
+        if($this->isAdministrator() || $this->isAdmin() || $this->isStaff()) {
+            return $this;
+        }
+    }
+
+    public function isUserType() {
+        if($this->isAdministrator()) {
+            return 'You are an Administrator';
+        } else if($this->isAdmin()) {
+            return 'You are Admin Staff';
+        } else if($this->isStaff()) {
+            return 'You are Staff';
+        } else {
+            return 'You are a Member';
+        }
+    }
+
+    public function activateAccount() {
+        $this->update([
+            'active' => true,
+            'locked' => false,
+            'active_hash' => null
+        ]);
+    }
+
+    public function updateRememberCredentials($identifier, $token) {
+        $this->update([
+            'remember_identifier' => $identifier,
+            'remember_token' => $token
+        ]);
+    }
+
+    public function removeRememberCredentials() {
+        $this->updateRememberCredentials(null, null);
+    }
+
+    public function createLoginToken($token) {
+        $this->update([
+            'token' => $token
+        ]);
+    }
+
+    public function removeLoginToken() {
+        $this->createLoginToken(null);
+    }
+
+    public function getFirstName() {
+        if($this->first_name) {
+            return $this->first_name;
+        }
+
+        return null;
+    }
+
+    public function getFullName() {
+        if($this->first_name && $this->last_name) {
+            return "{$this->first_name} {$this->last_name}";
+        }
+
+        if($this->first_name) {
+            return $this->first_name;
+        }
+
+        return null;
+    }
+
+    public function getNameOrUsername() {
+        return $this->getFullName() ?: $this->username;
+    }
+
+    public function getFirstNameOrUsername() {
+        return $this->first_name ?: $this->username;
+    }
 
 }
