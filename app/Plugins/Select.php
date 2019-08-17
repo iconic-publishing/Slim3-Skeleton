52e5e160e257df3a5c611e3ac2e1253e7b0e850b
John <john@custom-golf-bags.com>
2019-08-17 19:24:13 +0700

    Modified

--- app/Plugins/Select.php
+++ app/Plugins/Select.php
@@ -18,26 +18,26 @@ use Base\Plugins\Countries;
 
 class Select {
 	
-	public function title() {
-		return [
-			'Mr',
-			'Mrs',
-			'Ms',
-			'Miss'
-		];
-	}
-	
-	public function country() {
-		return Countries::get();
-	}
-	
-	public function department() {
-		return [
-			'General Enquiry',
-			'Order Enquiry',
-			'Shipping Enquiry',
-			'Other'
-		];
-	}
+    public function title() {
+        return [
+            'Mr',
+            'Mrs',
+            'Ms',
+            'Miss'
+        ];
+    }
+
+    public function country() {
+        return Countries::get();
+    }
+
+    public function department() {
+        return [
+            'General Enquiry',
+            'Order Enquiry',
+            'Shipping Enquiry',
+            'Other'
+        ];
+    }
 	
 }
