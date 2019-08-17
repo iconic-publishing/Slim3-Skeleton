7835a849d9ee29a6795ced9e6547501115ad0dc7
John <john@custom-golf-bags.com>
2019-08-17 18:16:52 +0700

    Modified

--- app/Controllers/BlogController.php
+++ app/Controllers/BlogController.php
@@ -15,8 +15,8 @@ Change Request ID:
 namespace Base\Controllers;
 
 use Base\{
-	Constructor\BaseConstructor,
-	Models\Blog
+    Constructor\BaseConstructor,
+    Models\Blog
 };
 use Psr\Http\Message\{
     ServerRequestInterface as Request,
@@ -25,23 +25,23 @@ use Psr\Http\Message\{
 
 class BlogController extends BaseConstructor {
 	
-	public function getBlogs(Request $request, Response $response) {
-		$blogs = Blog::orderBy('published_on', 'DESC')->paginate($this->config->get('blog.paginator'))->appends($request->getParams());
-		$sideBar = $this->sideBar();
-		
+    public function getBlogs(Request $request, Response $response) {
+        $blogs = Blog::orderBy('published_on', 'DESC')->paginate($this->config->get('blog.paginator'))->appends($request->getParams());
+        $sideBar = $this->sideBar();
+
         return $this->view->render($response, 'blog/blogs.php', compact('blogs', 'sideBar'));
     }
-	
-	public function getBlogDetails(Request $request, Response $response, $args) {
-		$slug = $args['slug'];
-		$blog = Blog::where('slug', $slug)->first();
-		$sideBar = $this->sideBar();
-		
+
+    public function getBlogDetails(Request $request, Response $response, $args) {
+        $slug = $args['slug'];
+        $blog = Blog::where('slug', $slug)->first();
+        $sideBar = $this->sideBar();
+
         return $this->view->render($response, 'blog/blog-details.php', compact('blog', 'sideBar'));
     }
-	
-	protected function sideBar() {
-		return Blog::inRandomOrder()->limit($this->config->get('blog.sideBarLimit'))->get();
-	}
+
+    protected function sideBar() {
+        return Blog::inRandomOrder()->limit($this->config->get('blog.sideBarLimit'))->get();
+    }
 	
 }
