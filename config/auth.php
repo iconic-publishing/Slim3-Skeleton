7cc659c259b885c0c65888c502c35e8c25b94f43
John <john@custom-golf-bags.com>
2019-08-18 13:22:50 +0700

    Modified

--- config/auth.php
+++ config/auth.php
@@ -14,21 +14,21 @@ Change Request ID:
 
 return [
 	
-	/*
-    |--------------------------------------------------------------------------
+    /*
+    |-----------------------------------------------------------------
     | Authentication Settings
-    |--------------------------------------------------------------------------
+    |-----------------------------------------------------------------
     |
     | All configurations are done via the .env file within the root.
 	| DO NOT MAKE ANY CHANGES HERE.
     |
     */
 	
-	'auth' => [
-		'remember' => 'user_r',
-		'token' => getenv('AUTH_TOKEN', 32),
-		'register' => getenv('AUTH_REGISTER', 32),
-		'recover' => getenv('AUTH_RECOVER', 32)
-	]
+    'auth' => [
+        'remember' => 'user_r',
+        'token' => getenv('AUTH_TOKEN', 32),
+        'register' => getenv('AUTH_REGISTER', 32),
+        'recover' => getenv('AUTH_RECOVER', 32)
+    ]
 
 ];
