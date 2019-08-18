0f519669a46f3a1d5aac297891bb2a299562dba8
John <john@custom-golf-bags.com>
2019-08-18 13:50:20 +0700

    Modified

--- config/database.php
+++ config/database.php
@@ -14,29 +14,29 @@ Change Request ID:
 
 return [
 	
-	/*
-    |--------------------------------------------------------------------------
+    /*
+    |-----------------------------------------------------------------
     | Database Connections
-    |--------------------------------------------------------------------------
+    |-----------------------------------------------------------------
     |
     | All configurations are done via the .env file within the root.
-	| DO NOT MAKE ANY CHANGES HERE.
+    | DO NOT MAKE ANY CHANGES HERE.
     |
     */
 	
-	'database' => [
-		'driver' => getenv('DB_DRIVER', 'mysql'),
-		'host' => getenv('DB_HOST', '127.0.0.1'),
-		'port' => getenv('DB_PORT', '3306'),
-		'database' => getenv('DB_DATABASE', 'skeleton'),
-		'username' => getenv('DB_USERNAME', 'root'),
-		'password' => getenv('DB_PASSWORD', ''),
-		'unix_socket' => getenv('DB_SOCKET', ''),
-		'charset' => 'utf8mb4',
-		'collation' => 'utf8mb4_unicode_ci',
-		'prefix' => '',
-		'strict' => true,
-		'engine' => null
-	]
+    'database' => [
+        'driver' => getenv('DB_DRIVER', 'mysql'),
+        'host' => getenv('DB_HOST', '127.0.0.1'),
+        'port' => getenv('DB_PORT', '3306'),
+        'database' => getenv('DB_DATABASE', 'skeleton'),
+        'username' => getenv('DB_USERNAME', 'root'),
+        'password' => getenv('DB_PASSWORD', ''),
+        'unix_socket' => getenv('DB_SOCKET', ''),
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'strict' => true,
+        'engine' => null
+    ]
 
 ];
