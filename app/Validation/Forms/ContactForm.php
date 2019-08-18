abe6ab04016a714f2c7da5853f881e3409ca9603
John <john@custom-golf-bags.com>
2019-08-18 12:27:17 +0700

    Modified

--- app/Validation/Forms/ContactForm.php
+++ app/Validation/Forms/ContactForm.php
@@ -20,14 +20,14 @@ class ContactForm {
 	
     public static function contactRules() {
         return [
-			'first_name' => v::notEmpty()->alpha(),
-			'last_name' => v::notEmpty()->alpha(),
-			'email_address' => v::noWhitespace()->notEmpty()->email(),
-			'mobile_number' => v::notEmpty()->phone(),
-			'country' => v::notEmpty(),
-			'department' => v::notEmpty()->alpha(),
-			'subject' => v::notEmpty(),
-            'message' => v::notEmpty(),
+            'first_name' => v::notEmpty()->alpha(),
+            'last_name' => v::notEmpty()->alpha(),
+            'email_address' => v::noWhitespace()->notEmpty()->email(),
+            'mobile_number' => v::notEmpty()->phone(),
+            'country' => v::notEmpty(),
+            'department' => v::notEmpty()->alpha(),
+            'subject' => v::notEmpty(),
+            'message' => v::notEmpty()
         ];
     }
 	
