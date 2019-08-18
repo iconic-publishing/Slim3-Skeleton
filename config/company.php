00ce346248202f7c34801c3ecb74e82f9596b135
John <john@custom-golf-bags.com>
2019-08-18 13:25:11 +0700

    Modified

--- config/company.php
+++ config/company.php
@@ -14,28 +14,28 @@ Change Request ID:
 
 return [
 	
-	/*
-    |--------------------------------------------------------------------------
+    /*
+    |-----------------------------------------------------------------
     | Project Settings
-    |--------------------------------------------------------------------------
+    |-----------------------------------------------------------------
     |
     | All configurations are done via the .env file within the root.
-	| DO NOT MAKE ANY CHANGES HERE TO THE 'contactFormEmail'.
-	|
-	| Changes to 'name', 'phone', 'address1', 'address2', 'address3', 'address4'
-	| and 'email' can be done here. More fields can be added to fit your project.
+    | DO NOT MAKE ANY CHANGES HERE TO THE 'contactFormEmail'.
+    |
+    | Changes to 'name', 'phone', 'address1', 'address2', 'address3', 'address4'
+    | and 'email' can be done here. More fields can be added to fit your project.
     |
     */
 	
-	'company' => [
-		'name' => getenv('COMPANY_NAME', 'Iconic Publishing Co Ltd'),
-		'phone' => getenv('COMPANY_PHONE', '+66 (0)33 005 922'),
-		'address1' => getenv('COMPANY_ADDRESS_1', 'Address Line 1'),
+    'company' => [
+        'name' => getenv('COMPANY_NAME', 'Iconic Publishing Co Ltd'),
+        'phone' => getenv('COMPANY_PHONE', '+66 (0)33 005 922'),
+        'address1' => getenv('COMPANY_ADDRESS_1', 'Address Line 1'),
         'address2' => getenv('COMPANY_ADDRESS_2', 'Address Line 2'),
         'address3' => getenv('COMPANY_ADDRESS_3', 'Address Line 3'),
         'address4' => getenv('COMPANY_ADDRESS_4', 'Address Line 4'),
-		'email' => getenv('COMPANY_EMAIL', 'info@example.com'),
-		'contactFormEmail' => getenv('CONTACT_FORM_EMAIL', 'enquiries@example.com')
-	]
+        'email' => getenv('COMPANY_EMAIL', 'info@example.com'),
+        'contactFormEmail' => getenv('CONTACT_FORM_EMAIL', 'enquiries@example.com')
+    ]
 
 ];
