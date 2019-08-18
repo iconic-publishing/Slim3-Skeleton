20c19cad46b8731f6a837db1546449669d3140fd
John <john@custom-golf-bags.com>
2019-08-18 14:01:35 +0700

    Modified

--- config/tags.php
+++ config/tags.php
@@ -14,30 +14,30 @@ Change Request ID:
 
 return [
 	
-	/*
-    |--------------------------------------------------------------------------
+    /*
+    |-----------------------------------------------------------------
     | Page Tiltes and Descriptions
-    |--------------------------------------------------------------------------
+    |-----------------------------------------------------------------
     |
-    | Page Tiltes and Descriptions for each page added here to fit your project.
-	|
-	| Important for good SEO to stop duplicate tags been listed and ranked.
-	|
+    | Page Tiltes and Descriptions for each page added here to fit 
+    | your project.
+    |
+    | Important for good SEO to stop duplicate tags been listed and ranked.
+    |
     */
-	
-	'tags' => [
-		'index' => [
-			'title' => 'This is a title for index page',
-			'description' => 'This is a description for index page'
-		],
-		
-		/* Example About Us Page
-		'about' => [
-			'title' => 'This is a title for about us page',
-			'description' => 'This is a description for about us page'
-		],
-		*/
-		
-	]
+
+    'tags' => [
+        'index' => [
+            'title' => 'This is a title for index page',
+            'description' => 'This is a description for index page'
+        ],
+
+        /* Example About Us Page
+        'about' => [
+            'title' => 'This is a title for about us page',
+            'description' => 'This is a description for about us page'
+        ],
+        */
+    ]
 
 ];
